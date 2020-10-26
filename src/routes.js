b916cffec1089feedf888a273764ff9b6d328b52
aniketcodes <aniketsinha1304@gmail.com>
2020-10-26 12:41:19 +0530

    Review A Product | issue in frontend resolved

--- src/routes.js
+++ src/routes.js
@@ -3,11 +3,13 @@ import PartnerPincode from './components/PartnerPincode';
 import ProductSlider from './components/ProductSlider';
 import RatingAndReview from './components/RatingAndReview';
 import ViewAllReviews from './components/ViewAllReviews';
+import ReviewAPartner from './components/ReviewAPartner';
 
 export const routes = [
     {path: '/productslider', component: ProductSlider},
     {path: '/buyagain', component: BuyAgain},
     {path: '/partnerpincode', component: PartnerPincode},
     {path: '/ratingAndReview',component: RatingAndReview},
-    {path:"/viewAllReviews",component: ViewAllReviews}
+    {path:"/viewAllReviews",component: ViewAllReviews},
+    {path:'/reviewAPartner',component:ReviewAPartner}
 ];
