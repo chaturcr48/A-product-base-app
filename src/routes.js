cc8b17b866a2b73305c50ba285b50ff50f7074af
Anshul Sharma <anshulsharma1610@gmail.com>
2020-10-26 13:05:41 +0530

    rating&review fix

--- src/routes.js
+++ src/routes.js
@@ -1,27 +1,13 @@
 import BuyAgain from './components/BuyAgain';
 import PartnerPincode from './components/PartnerPincode';
 import ProductSlider from './components/ProductSlider';
-<<<<<<< HEAD
-import ProductListDetail from './components/ProductListDetail';
-import ProductList from './components/ProductList';
-import MyWishlist from './components/MyWishlist';
-import Category from './components/Category';
-=======
 import RatingAndReview from './components/RatingAndReview';
 import ViewAllReviews from './components/ViewAllReviews';
->>>>>>> 084c66455029238e1243fe9f702717e437935843
 
 export const routes = [
     {path: '/productslider', component: ProductSlider},
     {path: '/buyagain', component: BuyAgain},
     {path: '/partnerpincode', component: PartnerPincode},
-<<<<<<< HEAD
-    {path: '/productlist/detail', component: ProductListDetail},
-    {path: '/productlist', component: ProductList},
-    {path: '/mywishlist', component: MyWishlist},
-    {path: '/category', component: Category}
-=======
     {path: '/ratingAndReview',component: RatingAndReview},
     {path:"/viewAllReviews",component: ViewAllReviews}
->>>>>>> 084c66455029238e1243fe9f702717e437935843
 ];
