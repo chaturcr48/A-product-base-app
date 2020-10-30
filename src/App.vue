6a6ef1f480e456702939f7c4a25b74014604af97
Yash Salvi <salviyash1410@gmail.com>
2020-10-30 22:55:24 +0530

    apis updated

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,17 @@
 <template>
-  <v-app>
-    <v-main>
-      <Header/>
-      <keep-alive>
-        <router-view></router-view>
-      </keep-alive>
-    </v-main>
-  </v-app>
+  <div>
+    <v-app>
+      <SearchResultsPage />
+    </v-app>
+  </div>
 </template>
 
-
-
 <script>
-import Header from "./components/Header";
-// import ProductDetails from "./components/ProductDetails";
-// import ProfilePage from "./components/Profile-Page";
-// import MyWishlist from "./components/MyWishlist";
-// import SearchResultsPage from "./components/SearchResultsPage";
-// import SearchPage from "./components/SearchPage";
-
+import SearchResultsPage from "./components/SearchResultsPage";
 export default {
   name: "App",
-
   components: {
-    Header,
-    // ProductDetails,
-    // ProfilePage,
-    // MyWishlist,
-    // SearchResultsPage,
-    // SearchPage,
+    SearchResultsPage,
   },
-
-  data: () => ({
-    //
-  }),
-  
-}
+};
 </script>
