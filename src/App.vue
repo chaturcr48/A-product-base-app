5bf06f3f0e9e23224e0d3941841d77a7513082fb
Anshul Sharma <anshulsharma1610@gmail.com>
2020-11-02 13:32:54 +0530

    app.vue

--- src/App.vue
+++ src/App.vue
@@ -11,22 +11,12 @@
 
 <script>
 import Header from "./components/Header";
-// import ProductDetails from "./components/ProductDetails";
-// import ProfilePage from "./components/Profile-Page";
-// import MyWishlist from "./components/MyWishlist";
-// import SearchResultsPage from "./components/SearchResultsPage";
-// import SearchPage from "./components/SearchPage";
 
 export default {
   name: "App",
 
   components: {
     Header,
-    // ProductDetails,
-    // ProfilePage,
-    // MyWishlist,
-    // SearchResultsPage,
-    // SearchPage,
   },
 
   data: () => ({
